a7c55b62d774ae21dbf71e6ab434e5a94a11d6b5
Felix <12586850+imfelixlaw@users.noreply.github.com>
2023-08-20 01:20:35 +0800

    to clean up windows event viewer log

---
+++ clear_event_log.ps1
@@ -0,0 +1,3 @@
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+
+wevtutil el | Foreach-Object {Write-Host "Clearing $_"; wevtutil cl "$_"}
